18587427c87e2618637633971c7c962f7f9ce933
a.panasenko <andrii.panasenko295@gmail.com>
2023-11-16 19:08:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,7 +20,7 @@ const CONTACTS_KEY = 'saved-contacts';
 const getStorageContacts = () => {
   const savedContacts = localStorage.getItem(CONTACTS_KEY);
 
-  return savedContacts === '' ? initialContacts : JSON.parse(savedContacts);
+  return savedContacts === '' ?JSON.parse(savedContacts) : initialContacts;
 };
 
 export const App = () => {
